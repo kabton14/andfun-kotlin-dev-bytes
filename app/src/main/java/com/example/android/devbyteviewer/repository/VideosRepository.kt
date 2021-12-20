5437eaf64bd924a024589214adcb55743ea0a670
Kevin Beaton <kabton14@gmail.com>
2021-12-20 10:12:13 -0500

    Create a repository class

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,9 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import com.example.android.devbyteviewer.database.VideosDatabase
+
+class VideosRepository(private val database: VideosDatabase) {
+
+}
