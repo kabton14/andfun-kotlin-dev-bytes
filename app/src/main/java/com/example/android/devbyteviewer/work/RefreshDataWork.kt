f8035b10fa9dcb73472d6ee1a7fabfe63deb7aea
Kevin Beaton <kabton14@gmail.com>
2021-12-25 22:59:52 -0500

    Override doWork function

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -20,8 +20,14 @@ package com.example.android.devbyteviewer.work
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
 
 class RefreshDataWork(appContext: Context, params: WorkerParameters):
         CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
 
-        }
+    }
+}
